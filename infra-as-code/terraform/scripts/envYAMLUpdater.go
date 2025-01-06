c16c802808394e75c2cee58e89dc604ad9af667b
nikhilmulinti <nikhil.mulinti@egovernments.org>
2025-01-06 13:26:17 +0530

    updating envyaml updater script

--- infra-as-code/terraform/scripts/envYAMLUpdater.go
+++ infra-as-code/terraform/scripts/envYAMLUpdater.go
@@ -35,7 +35,7 @@ func main() {
 		os.Exit(1)
 	}
 	// Read the YAML file
-	yamlFile, err := ioutil.ReadFile("../../../deploy-as-code/helm/charts/environments/egov-demo.yaml")
+	yamlFile, err := ioutil.ReadFile("../../../deploy-as-code/helm/charts/environments/env.yaml")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading YAML file: %v\n", err)
 		os.Exit(1)
@@ -48,7 +48,7 @@ func main() {
 	// Write the updated YAML to stdout
 	fmt.Println(output)
 
-	err = ioutil.WriteFile("../../../deploy-as-code/helm/charts/environments/egov-demo.yaml", []byte(output), 0644)
+	err = ioutil.WriteFile("../../../deploy-as-code/helm/charts/environments/env.yaml", []byte(output), 0644)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error writing YAML file: %v\n", err)
 		os.Exit(1)
